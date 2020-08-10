6317821fdb6b08624ebb0af5acf05a5c6089da55
Diego Ortega <rhaegarcode@gmail.com>
2020-08-10 05:31:44 -0500

    Dashboard Rhaegar Code

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const boton = document.getElementById('boton-sidebar')
+const html = document.getElementById('html')
+const botonSidebarLeft = document.getElementById('boton-sidebar_left')
+const navbar = document.getElementById('content-buttons')
+const botonFixed = document.getElementById('content-button_fixed')
+const sidebar = document.getElementById('sidebar')
+const contenedor = document.getElementById('main')
+
+boton.addEventListener('click', function() {
+    sidebar.classList.toggle('toggle');
+    contenedor.classList.toggle('main');
+    botonFixed.classList.remove('block');
+    botonFixed.classList.add('align');
+})
+
+botonFixed.addEventListener('click', function() {
+    sidebar.classList.toggle('toggle');
+    contenedor.classList.toggle('main')
+})
+
+botonSidebarLeft.addEventListener('click', function() {
+    html.classList.toggle('nav-open');
+})
+
+function mostrarSidebarTablet() {
+    let width = document.documentElement.offsetWidth
+    if( width < 1200) {
+        html.classList.add('nav-open');
+    }else {
+        html.classList.remove('nav-open');
+    }
+}
+
+function mostrarBotonFixed() {
+    let scroll = document.documentElement.scrollTop
+    console.log(scroll)
+    if(scroll >= 25) {
+        navbar.classList.add('none');
+        botonFixed.classList.add('block');
+    } else {
+        navbar.classList.remove('none');
+        botonFixed.classList.remove('block');
+    }
+}
+
+
+window.addEventListener('scroll', mostrarBotonFixed);
+window.addEventListener('resize', mostrarSidebarTablet);
